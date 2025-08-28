12e49900748892bae6078e90617f0dab34d9e0d4
Kevin <kgarange@uci.edu>
2025-08-28 19:32:34 +0000

    fix duplicate include

--- test/constitutive-model-energy-gradient.jl
+++ test/constitutive-model-energy-gradient.jl
@@ -1,40 +1,36 @@
+# Norma: Copyright 2025 National Technology & Engineering Solutions of
+# Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS,
+# the U.S. Government retains certain rights in this software. This software
+# is released under the BSD license detailed in the file license.txt in the
+# top-level Norma.jl directory.
 using Random
 
 include("../src/minitensor.jl")
-include("../src/constitutive_def.jl")
+include("../src/simulation.jl")
 include("../src/constitutive.jl")
 
-function finite_difference(material::Solid, F::Matrix{Float64}, dF::Matrix{Float64}, h::Float64)
-    return (constitutive(material, F - 2*h*dF)[1] - 8*constitutive(material, F - h*dF)[1] + 8*constitutive(material, F + h*dF)[1] - constitutive(material, F + 2*h*dF)[1]) / (12*h)
+function finite_difference(material::Solid, F::SMatrix{3,3,Float64}, dF::SMatrix{3,3,Float64}, h::Float64)
+    return (
+        constitutive(material, F - 2 * h * dF)[1] - 8 * constitutive(material, F - h * dF)[1] +
+        8 * constitutive(material, F + h * dF)[1] - constitutive(material, F + 2 * h * dF)[1]
+    ) / (12 * h)
 end
 
-
-@testset "constitutive-model-energy-gradient" begin
+@testset "Constitutive Model Energy Gradient" begin
     Random.seed!(0)
 
-    base_params = Dict{Any, Any}(
-          "elastic modulus" => 1.0,
-          "Poisson's ratio" => 0.3,
-          "density" => 1.0,
-    )
-    sh_params = merge(base_params, Dict{Any, Any}(
-          "m" => 2,
-          "n" => 2,
-         ))
-    models = [
-              Neohookean(base_params),
-              SaintVenant_Kirchhoff(base_params),
-              SethHill(sh_params),
-             ]
+    base_params = Parameters("elastic modulus" => 1.0, "Poisson's ratio" => 0.3, "density" => 1.0)
+    sh_params = merge(base_params, Parameters("m" => 2, "n" => 2))
+    models = [Neohookean(base_params), SaintVenant_Kirchhoff(base_params), SethHill(sh_params)]
     F_n = 10
     dF_n = 10
 
     for model in models
         for _ in 1:F_n
-            F = Random.randn(3, 3) * 0.1 + I
+            F = SMatrix{3,3,Float64}(randn(3,3) * 0.1 + I)
             dWdF = constitutive(model, F)[2]
             for _ in 1:dF_n
-                dF = Random.randn(3, 3) * 0.1
+                dF = SMatrix{3,3,Float64}(randn(9) * 0.1)
                 dWdF_fd = finite_difference(model, F, dF, 1e-6)
                 @test isapprox(sum(dWdF .* dF), dWdF_fd, atol=1e-6)
             end
